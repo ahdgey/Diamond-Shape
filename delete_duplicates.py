a544cb2207cce1999e960ea1596ac97c2debd9f7
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-10-02 18:45:10 +0800

    The function is working.

--- delete_duplicates.py
+++ delete_duplicates.py
@@ -1,4 +1,28 @@
+# Alexza Jean R. Catanoy
+# BSCPE 2-4
+# Lab Activity: Delete Duplicates
 
+# Define the function
+def delete_duplicates(input_str):
 
+    # Create a blank string from scratch to save the output
+    output = ""
 
+    for char in input_str:
+        if input_str.count(char) == 1:
+            output += char
 
+    return output
+
+try:
+    # See how many strings are there
+    n = int(input("Type in the number of strings: "))
+
+    # Take each string and process it
+    for _ in range(n):
+        input_str = input("Type in a word: ")
+        output_str = delete_duplicates(input_str)
+        print("The new word is: " + output_str)
+
+except ValueError:
+    print("As the first input, kindly stype in a valid integer.")
