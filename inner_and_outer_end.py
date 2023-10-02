0abd893c558c2fc7a154934c54d3e255480cfa2a
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-10-02 18:54:35 +0800

    The output has been seen.

---
+++ inner_and_outer_end.py
@@ -0,0 +1,13 @@
+# Alexza Jean R. Catanoy
+# BSCPE 2-4
+# Lab Activity: Inner and Outer End
+
+for i in range(2):
+    for j in range(2):
+        print(i, j)
+    
+    else:
+        print("Inner End")
+
+else:
+    print("Outer End")
